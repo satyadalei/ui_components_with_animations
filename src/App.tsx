59c86a70a15340a800267e24e242aab2904f863b
Satyanarayan Dalei <satyanarayandalei65@gmail.com>
2025-03-21 00:14:22 +0530

    Added a vertical thin gradient line

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1 className="">Hello</h1>
+        <hr />
+        {/* <div className="h-[80vh] border border-red-500 flex justify-center items-center">
+          <div className="border h-[80%] w-5 relative overflow-hidden">
+            <div className="w-[300px] h-[300px] rounded-full bg-radial from-sky-400/70 via-sky-400/20 to-transparent absolute inset-1/2 -translate-x-1/2 -translate-y-1/2"></div>
+          </div>
+        </div> */}
+
+        <div className="h-[80vh] flex justify-center items-center relative">
+          {/* Thin Vertical Line */}
+          <div className="w-px h-full relative">
+            {/* Glowing Gradient Effect */}
+            {/* <div className="absolute inset-1/2 -translate-x-1/2 -translate-y-1/2 w-[300px] h-[300px] bg-gradient-radial from-purple-500/70 via-indigo-400/30 to-transparent blur-3xl"></div> */}
+
+            <div className="h-full absolute inset-1/2 -translate-x-1/2 -translate-y-1/2 w-px bg-gradient-to-b from-transparent from-5% via-blue-500 via-50% to-transparent to-5%"></div>
+          </div>
+        </div>
+
+        <hr />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
