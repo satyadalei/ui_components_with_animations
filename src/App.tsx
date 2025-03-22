7d81ad2bf51c7c363bfb9221c896da3b6889df48
Satyanarayan Dalei <satyanarayandalei65@gmail.com>
2025-03-22 18:50:49 +0530

    Added Scroll animation & text reveal

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,25 @@
 import "./App.css";
+import Introduction from "./components/Introduction";
 
 function App() {
   return (
     <>
       <div>
-        <h1 className="">Hello</h1>
         <hr />
-        {/* <div className="h-[80vh] border border-red-500 flex justify-center items-center">
-          <div className="border h-[80%] w-5 relative overflow-hidden">
-            <div className="w-[300px] h-[300px] rounded-full bg-radial from-sky-400/70 via-sky-400/20 to-transparent absolute inset-1/2 -translate-x-1/2 -translate-y-1/2"></div>
-          </div>
-        </div> */}
-
         <div className="h-[80vh] flex justify-center items-center relative">
           {/* Thin Vertical Line */}
           <div className="w-px h-full relative">
-            {/* Glowing Gradient Effect */}
-            {/* <div className="absolute inset-1/2 -translate-x-1/2 -translate-y-1/2 w-[300px] h-[300px] bg-gradient-radial from-purple-500/70 via-indigo-400/30 to-transparent blur-3xl"></div> */}
-
-            <div className="h-full absolute inset-1/2 -translate-x-1/2 -translate-y-1/2 w-px bg-gradient-to-b from-transparent from-5% via-blue-500 via-50% to-transparent to-5%"></div>
+            <div className="h-full absolute inset-1/2 -translate-x-1/2 -translate-y-1/2 w-px bg-gradient-to-b from-transparent via-blue-500 to-transparent">
+            </div>
           </div>
         </div>
 
+        {/* <div className="h-10 bg-gradient-to-r from-blue-500 from-10% via-green-500 via-30% to-yellow-500 to-90%" ></div>
+        <div className="h-10 bg-gradient-to-r from-transparent via-blue-500 via-80% to-transparent" ></div> */}
+
         <hr />
       </div>
+      <Introduction />
     </>
   );
 }
